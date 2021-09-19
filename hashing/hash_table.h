22fc55a5d0a2f785aed121ed51bc06fb5bd648fc
Rodion Sviridov <sviridov.rod@yandex.ru>
2021-09-19 17:59:50 +0300

    HashTable implemented

--- hashing/hash_table.h
+++ hashing/hash_table.h
@@ -5,59 +5,133 @@
 #define HASH_TABLE_H
 
 #include <vector>
+#include <iostream>
+#include <string>
+#include <exception>
 
-class Hashable {
-public:
-    virtual int getIndex() const = 0;
-};
-
-struct Reader : Hashable {
-    int id{};
+struct Reader {
+    int id{-1};
     std::string name{};
     std::string address{};
 
-    int getIndex() const override {
-        return id;
-    }
+	std::string toString() const {
+		return "Reader(id=" + std::to_string(id) + ",name=" + name + ",address=" + address +")";
+	}
 };
 
-template <class Hashable>
-class hash_table {
-private:
-    std::vector<Hashable> data;
+enum class TableEntityState {
+	FREE,
+	IN_USE
+};
 
-    int hash(const int& key) {
-        return key % data.size();
-    }
+template<typename Key_, typename Value_>
+struct TableEntity {
+	Key_ key;
+	Value_ value;
+	TableEntityState state = TableEntityState::FREE;
+};
+
+template<typename Key, typename Value, typename Hash>
+class HashTable {
+private:
+	using Table = TableEntity<Key, Value>*;
+	size_t capacity{};
+	size_t size{};
+
+	Table table;
+	Hash hashFunc;
+
+	size_t getIndex(const Key& key, const size_t& modulus) const {
+		return hashFunc(key) % modulus;
+	}
+
+	size_t squareGetIndex(const Key& key, const size_t& constant, const size_t& modulus) const {
+		return (hashFunc(key) + (constant + constant * constant) / 2) % modulus;
+	}
+
+	void rehash() {
+		size_t newCapacity = capacity * 2;
+
+		auto* newTable = new TableEntity<Key, Value>[newCapacity];
+		for (size_t i = 0; i < newCapacity; i++) {
+			newTable[i] = TableEntity<Key, Value>();
+		}
+
+		for (size_t i = 0; i < capacity; i++)
+			if (table[i].state == TableEntityState::IN_USE) {
+				insert(table[i].key, table[i].value, newTable,  newCapacity);
+			}
+
+		delete[] table;
+
+		table = newTable;
+		capacity = newCapacity;
+	}
+
+	size_t insert(const Key& key_, const Value& value_, Table table_, size_t capacity_) {
+		size_t index = getIndex(key_, capacity_);
+		for (size_t constant = 1; table_[index].state == TableEntityState::IN_USE; constant++) {
+			index = squareGetIndex(key_, constant, capacity_);
+		}
+		table_[index].key = key_;
+		table_[index].value = value_;
+		table_[index].state = TableEntityState::IN_USE;
+		return index;
+	}
 
 public:
-    void insert(const Hashable& item) {
-        int index = hash(item.getIndex());
-        if (index + 1 >= data.size()) {
-            data.resize(data.size() + index)
-        }
-    }
 
-    void erase(const Hashable& item) {
+	explicit HashTable(size_t capacity, Hash hashFunction)
+		: capacity(capacity), size(0), hashFunc(hashFunction)
+	{
+		table = new TableEntity<Key,Value>[capacity];
 
-    }
+		for (size_t i = 0; i < capacity; i++) {
+			table[i] = TableEntity<Key, Value>();
+		}
+	}
 
-    void find(const int& index) {
+	size_t insert(const Key& key, const Value& value) {
+		if (size * 2 > capacity) {
+			rehash();
+		}
+		size++;
+		return insert(key, value, table, capacity);
+	}
 
-    }
+	void erase(const Key& key) {
+		size_t index = getIndex(key, capacity);
+		for (size_t constant = 1; key != table[index].key; constant++) {
+			index = squareGetIndex(key, constant, capacity);
+		}
+		table[index].state = TableEntityState::FREE;
+		size--;
+	}
 
-    Type operator[] (const int& index) {
+	Value& operator[] (const Key& key) const {
+		size_t index = getIndex(key, capacity);
+		std::vector<bool> indexes(capacity, false);
+		for (size_t constant = 1; key != table[index].key; constant++) {
+			indexes[index] = true;
+			index = squareGetIndex(key, constant, capacity);
+			if (std::find(indexes.begin(), indexes.end(), false) == indexes.end())
+				throw std::invalid_argument("No entity with given index");
+		}
+		if (table[index].state == TableEntityState::IN_USE) return table[index].value;
+		throw std::invalid_argument("Entity was erased");
+	}
 
-    }
+	size_t getSize() const {
+		return size;
+	}
 
-    void size() const {
-        return data.size();
-    }
+	size_t getCapacity() const {
+		return capacity;
+	}
+
+	~HashTable() {
+		delete[] table;
+	}
 };
-
-template<class Hashable>
-ostream& operator<<(std::ostream& stream, const hash_table<Hasheble>& data) {
-
-}
 
 #endif //HASH_TABLE_H
--- hashing/main.cpp
+++ hashing/main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include <vector>
+#include <functional>
+#include "hash_table.h"
+
+using namespace std;
+
+int hashFunction(const int& key) {
+	return key % 7;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+	HashTable<int, Reader, std::function<int(const int&)> > table(4, hashFunction);
+	const size_t size = 10;
+	for (int i = 0; i < size; i++) {
+		table.insert(i, Reader{i, "NAME", "ADDRESS"});
+	}
+	for (int i = 0; i < size; i++) {
+		cout << table[i].toString() << endl;
+	}
+	table.erase(5);
+	cout << "---------------------" << endl;
+	for (int i = 0; i < size; i++) {
+		try {
+			cout << table[i].toString() << endl;
+		} catch (exception& e) {
+			cerr << e.what() << endl;
+		}
+
+	}
     return 0;
 }
